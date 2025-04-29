a3da96becd046bde175c41c0716bc76427a6d4d1
TalkFox <2002duguxiu@gmail.com>
2025-04-29 10:05:10 +0200

    Config Display.js

--- js/display.js
+++ js/display.js
@@ -1,3 +1,25 @@
 class Display{
+    constructor(nomjugador){
+        this.nomjugador = nomjugador;
+        this.puntuacio_jugador = 0;
+        this.puntuacio_ordinador = 0;
+    }
+
+    setPuntuacio(jugador){
+        if(jugador == 0){
+            this.puntuacio_jugador++;
+            $("score-jugador1").html(this.puntuacio_jugador);
+        }else{
+            this.puntuacio_ordinador++;
+            $("score-jugador2").html(this.puntuacio_ordinador);
+        }
+    }
+    getPuntuacio(jugador){
+        if(jugador == 0){
+            return this.puntuacio_jugador;
+        }else{
+            return this.puntuacio_ordinador;
+        }
+    }
 
 }
